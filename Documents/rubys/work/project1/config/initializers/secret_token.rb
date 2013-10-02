0e85e4f97864393199843ca91842484b05162691
Sam Ruby <ruby@intertwingly.net>
2013-10-02 18:25:10 -0400

    chapters 8-12 updated in project2 directory

---
+++ Documents/rubys/work/project1/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Project1::Application.config.secret_token = '1a6003ac3ff39090f8b62e72d94ba27f68cb22e9f8f2b021ff61966ddbef7ae92b466aee341ae9494f4db4f1581ea86b155dd67120fd546d01dcc409e48fec2e'
---
+++ Documents/rubys/work/project2/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Project2::Application.config.secret_token = '400cdc37306daf31952561a0aec41682040fa78d0eb3d787d95500369fcc057c74bac5bc05689f0fcbf01b948c8b664291c8cc2b753f3b8b06afc4e3ae1c163d'
